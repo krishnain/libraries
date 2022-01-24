5301a46eb6810f8dbce25f9b563e9fa2fd51d958
krishnain <86010658+krishnain@users.noreply.github.com>
2022-01-24 08:41:25 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,6 +10,8 @@ def newMaven()
 
 def newDeploy(ip,appname)
 {
+  echo "${ip}"
+  echo "${appname}"
     sh 'scp /home/ubuntu/.jenkins/workspace/DeclarativePipeline/webapp/target/webapp.war ubuntu@"${ip}":/var/lib/tomcat9/webapps/"${appname}".war'
 }
 
